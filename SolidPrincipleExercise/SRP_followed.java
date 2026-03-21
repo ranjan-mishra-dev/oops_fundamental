4e7a01434d949eef1c8895f1b67794f36cc06c62
Ranjan Mishra <ranjan.mishra.dev@gmail.com>
2026-03-21 18:31:33 +0530

    SRP first exercise done!

---
+++ SolidPrincipleExercise/SRP_followed.java
@@ -0,0 +1,32 @@
+public class SRP_followed {
+    public static void main(String[] args) {
+        // System.out.println("ranjan");
+
+        ReportGenerator rg = new ReportGenerator();
+        rg.generateReport();
+
+        ReportSaver rs = new ReportSaver();
+        rs.savedToFile();
+
+        ReportNotification rn = new ReportNotification();
+        rn.sendEmail();
+    }
+}
+
+class ReportGenerator {
+    public void generateReport() {
+        System.out.println("Generating report.");
+    }
+}
+
+class ReportSaver {
+    public void savedToFile() {
+        System.out.println("report saving in file.");
+    }
+}
+
+class ReportNotification {
+    public void sendEmail() {
+        System.out.println("Sending email notification.");
+    }
+}
